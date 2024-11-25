75b0e2b2f9650ae0e6e1c58efb0ea660823729c8
ip25 <48208226+IP577@users.noreply.github.com>
2024-11-25 22:38:20 +0530

    Add failing test for single number input

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -3,3 +3,7 @@ import { add } from "../stringCalculator.js";
 test("add: should return 0 for an empty string", () => {
   expect(add("")).toBe(0);
 });
+
+test("add: should return the number itself for single number input", () => {
+  expect(add("1")).toBe(1);
+});
